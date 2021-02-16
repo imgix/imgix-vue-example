d479bacb4457b46f57e4bded06296e78ee8933e0
Luis Ball <lball@imgix.com>
2021-02-16 10:51:19 -0500

    feat(imgix-vue): import and register imgix-vue library to app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import VueImgix from 'vue-imgix';
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueImgix, {
+  domain: "lball.imgix.net",
+  defaultIxParams: {
+    /* 
+     * This enables the auto format imgix parameter by default
+     * for all images, which we recommend to reduce image size, 
+     * but you might choose to turn this off.
+     */
+    auto: "format"
+  }
+});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
